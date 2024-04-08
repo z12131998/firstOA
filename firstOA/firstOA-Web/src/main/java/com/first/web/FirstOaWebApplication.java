83f8707cd6ed5a84184cac1d03ed949fefd791d6
zjy <1009967133@qq.com>
2024-04-08 15:23:36 +0800

    test

---
+++ firstOA/firstOA-Web/src/main/java/com/first/web/FirstOaWebApplication.java
@@ -0,0 +1,13 @@
+package com.first.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstOaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstOaWebApplication.class, args);
+    }
+
+}
---
+++ firstOA/firstOA-Web/src/test/java/com/first/web/FirstOaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstOaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
